93c1ff74586e59ae6fa024aebe8de24bae658790
sayed <sayedmasnouer222@gmail.com>
2026-05-24 02:31:36 +0300

    fix:check all files and add a hide options

--- src/DB/model/message.model.js
+++ src/DB/model/message.model.js
@@ -9,7 +9,7 @@ const messageSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
-    isPublic: { type: Boolean, default: false },
+    isPublic: { type: Boolean, default: true },
 
 
 
